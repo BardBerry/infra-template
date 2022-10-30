802f08d2fb8efdf40b689027a06ce00ea282ef3d
BardBerry <bardberry@yandex.ru>
2022-10-30 09:42:05 +0300

    test action

---
+++ test.ts
@@ -0,0 +1,11 @@
+// var a = 'no-var';
+
+// console.log(a);
+
+// const str: string = 'testing';
+
+// function test1(str: string, str2: string): string {
+//   return str.toUpperCase();
+// }
+
+// test1(str);
