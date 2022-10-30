ffa9e3efa37b1d75254c97977bd4bb280f65a282
BardBerry <bardberry@yandex.ru>
2022-10-30 10:02:18 +0300

    test action without lint errors

--- test.ts
+++ test.ts
@@ -1,11 +1,3 @@
-var a = 'no-var';
+// var a = 'no-var';
 
-console.log(a);
-
-// const str: string = 'testing';
-
-// function test1(str: string, str2: string): string {
-//   return str.toUpperCase();
-// }
-
-// test1(str);
+// console.log(a);
