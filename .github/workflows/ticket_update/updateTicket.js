4fc326976361cd1a9d7c2420aaa26faa7034faac
BardBerry <bardberry@yandex.ru>
2022-10-30 12:57:58 +0300

    test script

---
+++ .github/workflows/ticket_update/updateTicket.js
@@ -0,0 +1,20 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+async function getAllCommits() {
+  try {
+    const {OAUTH, ACTOR, RELEASE, ORG_ID} = process.env;
+    const response = await fetch('https://api.github.com/repos/BardBerry/infra-template/compare/rc-0.0.1...rc-0.0.6');
+    if (response.ok) {
+      const data = await response.json();
+      const commits = data.commits.map((el) => {
+        return `${el.commit.author.name} - ${el.commit.message}`;
+      }).join('\n');
+
+      console.log(commits);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getAllCommits();
