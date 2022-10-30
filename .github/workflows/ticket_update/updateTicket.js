d651ff03f784efb0f45b6fd3737b0b1061416b2d
BardBerry <bardberry@yandex.ru>
2022-10-30 15:46:46 +0300

    one tag

--- .github/workflows/ticket_update/updateTicket.js
+++ .github/workflows/ticket_update/updateTicket.js
@@ -6,15 +6,16 @@ async function getAllTags() {
     if (response.ok) {
       const data = await response.json();
       if (data.length < 2) {
-        // const res = await fetch('https://api.github.com/repos/BardBerry/infra-template/git/refs/tags');
+        getAllCommits();
       } else {
-        // const regex = /rc-0.0.[0-9]+/;
-        let firstTag = data.pop().url;
-        // firstTag = firstTag.ref
-        console.log(firstTag);
-        // firstTag = regex.exec(firstTag);
-        // console.log(firstTag);
+        const regex = /rc-0.0.[0-9]+/;
+        let lastTag = regex.exec(data.pop().ref)[0];
+        let tagBeforeLast = regex.exec(data.pop().ref)[0];
+        getAllCommits(lastTag, tagBeforeLast);
       }
+    }
+    else {
+      getAllCommits();
     }
   } catch (error) {
     console.log(error);
@@ -22,17 +23,29 @@ async function getAllTags() {
 }
 
 
-async function getAllCommits() {
+async function getAllCommits(lastTag = '', tagBeforeLast = '') {
   try {
     const {OAUTH, ACTOR, RELEASE, ORG_ID} = process.env;
-    const response = await fetch('https://api.github.com/repos/BardBerry/infra-template/compare/rc-0.0.1...rc-0.0.6');
-    if (response.ok) {
-      const data = await response.json();
-      const commits = data.commits.map((el) => {
-        return `${el.commit.author.name} - ${el.commit.message}`;
-      }).join('\n');
-
-      console.log(commits);
+    if (lastTag && tagBeforeLast) {
+      const response = await fetch('https://api.github.com/repos/BardBerry/infra-template/compare/rc-0.0.1...rc-0.0.6');
+      if (response.ok) {
+        const data = await response.json();
+        const commits = data.commits.map((el) => {
+          return `${el.commit.author.name} - ${el.commit.message}`;
+        }).join('\n');
+  
+        console.log(commits);
+      }
+    } else {
+      const response = await fetch('https://api.github.com/repos/BardBerry/infra-template/commits');
+      if (response.ok) {
+        const data = await response.json();
+        const commits = data.map((el) => {
+          return `${el.sha} ${el.commit.author.name} ${el.commit.message}`;
+        }).join('\n');
+  
+        console.log(commits);
+      }
     }
   } catch (error) {
     console.log(error);
